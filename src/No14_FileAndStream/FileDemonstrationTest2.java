b35b1b7c7d94cde14f3ae94451581223a888b167
Ysuper <Ysuper@Ysuper-PC>
2017-12-12 00:33:25 +0800

    No13_Exception & No14_FileAndStream

---
+++ src/No14_FileAndStream/FileDemonstrationTest2.java
@@ -0,0 +1,29 @@
+package No14_FileAndStream;
+
+// Fig. 14.23: FileDemonstrationTest.java
+// Testing the FileDmonstration class.
+import javax.swing.JFrame;
+
+public class FileDemonstrationTest2
+{
+   public static void main( String args[] )
+   {
+      FileDemonstration2 application = new FileDemonstration2();
+      application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+   } // end main
+} // end class FileDemonstrationTest
+
+/*************************************************************************
+* (C) Copyright 1992-2007 by Deitel & Associates, Inc. and               *
+* Pearson Education, Inc. All Rights Reserved.                           *
+*                                                                        *
+* DISCLAIMER: The authors and publisher of this book have used their     *
+* best efforts in preparing the book. These efforts include the          *
+* development, research, and testing of the theories and programs        *
+* to determine their effectiveness. The authors and publisher make       *
+* no warranty of any kind, expressed or implied, with regard to these    *
+* programs or to the documentation contained in these books. The authors *
+* and publisher shall not be liable in any event for incidental or       *
+* consequential damages in connection with, or arising out of, the       *
+* furnishing, performance, or use of these programs.                     *
+*************************************************************************/
